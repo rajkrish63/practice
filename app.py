c4a23346cb63a28bb04e65b0af024b9bfdfc1b9a
Rajesh <144893908+rajkrish63@users.noreply.github.com>
2024-04-11 20:17:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,24 +9,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/scrape', methods=['GET', 'POST'])
+@app.route('/scrape', methods=['POST'])
 def scrape():
-    if request.method == 'POST':
-        url = request.json['url']
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+    url = request.json['url']
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-        text = soup.get_text()
+    text = soup.get_text()
 
-        images = soup.find_all('img')
-        images_data = [{'src': img['src']} for img in images]
+    images = soup.find_all('img')
+    images_data = [{'src': img['src']} for img in images]
 
-        videos = soup.find_all('video')
-        videos_data = [{'src': video['src']} for video in videos]
+    videos = soup.find_all('video')
+    videos_data = [{'src': video['src']} for video in videos]
 
-        return jsonify({'text': text, 'images': images_data, 'videos': videos_data})
-    else:
-        return render_template('index.html')  # Return the index.html page for GET requests
+    return jsonify({'text': text, 'images': images_data, 'videos': videos_data})
 
 if __name__ == '__main__':
     app.run(debug=True)
